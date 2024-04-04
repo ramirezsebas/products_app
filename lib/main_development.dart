3263275287cbd7332b205ef31183161240ed7f3e
Matias Ramirez <ramirezmatias946@gmail.com>
2024-04-04 12:14:40 -0400

    Remove unnecessary import statement in main_development.dart

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,7 +1,6 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:product_manager/app/app.dart';
 import 'package:product_manager/bootstrap.dart';
 
 void main() {
-  bootstrap(() => const ProviderScope(child: App()));
+  bootstrap(() => const App());
 }
