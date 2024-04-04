74a0148761c67321b5c13028232d57217d5577ec
Matias Ramirez <ramirezmatias946@gmail.com>
2024-04-04 13:50:07 -0400

    Refactor product screen layout and add product view section

--- lib/app/screens/product_screen.dart
+++ lib/app/screens/product_screen.dart
@@ -1,10 +1,8 @@
-import 'package:card_swiper/card_swiper.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:product_manager/app/models/product_model.dart';
 import 'package:product_manager/app/providers/providers.dart';
+import 'package:product_manager/app/widgets/product_view_section.dart';
 
 class ProductScreen extends ConsumerWidget {
   const ProductScreen({
@@ -23,6 +21,12 @@ class ProductScreen extends ConsumerWidget {
             content: Text('Product added to favorites'),
           ),
         );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Product removed from favorites'),
+          ),
+        );
       }
     });
 
@@ -39,38 +43,7 @@ class ProductScreen extends ConsumerWidget {
       body: CustomScrollView(
         slivers: [
           SliverToBoxAdapter(
-            child: Column(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  height: 300,
-                  child: Swiper(
-                    itemCount: productModel.images.length,
-                    itemBuilder: (context, index) {
-                      return Card(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image.network(
-                              productModel.images[index],
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                    pagination: const SwiperPagination(),
-                    controller: SwiperController(),
-                    autoplay: true,
-                  ),
-                ),
-                ListTile(
-                  title: Text(productModel.title),
-                  subtitle: Text(productModel.description),
-                  trailing: Text(productModel.price.toString()),
-                ),
-              ],
-            ),
+            child: ProductViewSection(productModel: productModel),
           ),
         ],
       ),
