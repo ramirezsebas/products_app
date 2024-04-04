6a5c232dac5c93f1bd107111d9b269511333a1d4
Matias Ramirez <ramirezmatias946@gmail.com>
2024-04-04 12:14:31 -0400

    Update bootstrap.dart to use ProviderScope from flutter_riverpod

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,13 +2,12 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
-  // Add cross-flavor configuration here
-
-  runApp(await builder());
+  runApp(ProviderScope(child: await builder()));
 }
