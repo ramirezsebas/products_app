ebec5037768b9a47cf89578290fc00302e21602e
Matias Ramirez <ramirezmatias946@gmail.com>
2024-04-04 14:46:24 -0400

    Added the full image screen

--- lib/app/core/router.dart
+++ lib/app/core/router.dart
@@ -2,12 +2,14 @@ import 'package:go_router/go_router.dart';
 import 'package:product_manager/app/models/product_model.dart';
 import 'package:product_manager/app/screens/error_screen.dart';
 import 'package:product_manager/app/screens/favorites_screen.dart';
+import 'package:product_manager/app/screens/full_image_screen.dart';
 
 import 'package:product_manager/app/screens/product_screen.dart';
 import 'package:product_manager/app/screens/products_screen.dart';
 
 final router = GoRouter(
   initialLocation: '/products',
+  debugLogDiagnostics: true,
   routes: [
     GoRoute(
       path: '/products',
@@ -32,6 +34,22 @@ final router = GoRouter(
               productModel: extra,
             );
           },
+          routes: [
+            GoRoute(
+              path: 'image',
+              builder: (context, state) {
+                final extra = state.extra as String?;
+
+                final image = extra;
+
+                if (image == null) {
+                  return const ErrorScreen();
+                }
+
+                return FullImageScreen(image: image);
+              },
+            ),
+          ],
         ),
       ],
     ),
