1d10b1b9353c42c5460f314d9d1c77cc95f93efa
Matias Ramirez <ramirezmatias946@gmail.com>
2024-04-04 13:08:09 -0400

    Add UsersRepository class

---
+++ lib/app/repositories/users_repository.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:product_manager/app/models/user_model.dart';
+
+class UsersRepository {
+  UsersRepository({required this.client});
+
+  final Dio client;
+
+  Future<UserModel> getRandomUser(String id) async {
+    try {
+      final response = await client.get('/users/$id');
+
+      final responseData = response.data as Map<String, dynamic>;
+
+      final user = UserModel.fromJson(responseData);
+
+      return user;
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
