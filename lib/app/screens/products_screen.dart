e80520d91b76049ffc66fa4e7e57e3981f9b8bff
Matias Ramirez <ramirezmatias946@gmail.com>
2024-04-04 16:51:08 -0400

    Refactor ProductsScreen to use SliverChildBuilderDelegate for better performance

--- lib/app/screens/products_screen.dart
+++ lib/app/screens/products_screen.dart
@@ -4,13 +4,13 @@ import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:go_router/go_router.dart';
 import 'package:product_manager/app/providers/providers.dart';
 import 'package:product_manager/app/widgets/favorite_icon_button.dart';
+import 'package:shimmer/shimmer.dart';
 
 class ProductsScreen extends ConsumerWidget {
   const ProductsScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final products = ref.watch(fetchProductsProvider());
     final user = ref.watch(fetchRandomUserProvider);
 
     return SafeArea(
@@ -54,74 +54,92 @@ class ProductsScreen extends ConsumerWidget {
             const SliverToBoxAdapter(
               child: Divider(),
             ),
-            products.when(
-              data: (data) {
-                return SliverList.separated(
-                  itemCount: data.length,
-                  separatorBuilder: (context, index) => const Divider(),
-                  itemBuilder: (context, index) {
-                    final product = data[index];
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  const limit = 10;
+                  final skip = index ~/ limit;
 
-                    return Slidable(
-                      key: ValueKey(product.id),
-                      endActionPane: ActionPane(
-                        motion: const ScrollMotion(),
-                        children: [
-                          SlidableAction(
-                            onPressed: (context) {},
-                            backgroundColor: const Color(0xFF21B7CA),
-                            foregroundColor: Colors.white,
-                            icon: Icons.favorite_rounded,
-                            label: 'Add to Favorite',
-                          ),
-                        ],
-                      ),
-                      child: ListTile(
-                        leading: CircleAvatar(
-                          backgroundImage: NetworkImage(product.thumbnail),
+                  final itemIndexInProducts = index % limit;
+
+                  final products = ref.watch(
+                    fetchProductsProvider(
+                      limit: limit,
+                      skip: skip,
+                    ),
+                  );
+
+                  return products.when(
+                    data: (data) {
+                      if (itemIndexInProducts >= data.length) {
+                        return null;
+                      }
+
+                      final product = data[itemIndexInProducts];
+                      return Slidable(
+                        key: ValueKey(product.id),
+                        endActionPane: ActionPane(
+                          motion: const ScrollMotion(),
+                          children: [
+                            SlidableAction(
+                              onPressed: (context) {},
+                              backgroundColor: const Color(0xFF21B7CA),
+                              foregroundColor: Colors.white,
+                              icon: Icons.favorite_rounded,
+                              label: 'Add to Favorite',
+                            ),
+                          ],
                         ),
-                        title: Text(product.title),
-                        subtitle: Text(product.description),
-                        trailing: IconButton(
-                          icon: Icon(
-                            !ref
-                                    .watch(
-                                      setFavoriteProductsProvider.notifier,
-                                    )
-                                    .isFavorite(product)
-                                ? Icons.favorite_border_rounded
-                                : Icons.favorite_rounded,
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(product.thumbnail),
                           ),
-                          onPressed: () {
+                          title: Text(product.title),
+                          subtitle: Text(product.description),
+                          trailing: IconButton(
+                            icon: Icon(
+                              !ref
+                                      .watch(
+                                        setFavoriteProductsProvider.notifier,
+                                      )
+                                      .isFavorite(product)
+                                  ? Icons.favorite_border_rounded
+                                  : Icons.favorite_rounded,
+                            ),
+                            onPressed: () {
+                              ref
+                                  .read(setFavoriteProductsProvider.notifier)
+                                  .addToFavorites(product);
+                            },
+                          ),
+                          onTap: () {
                             ref
-                                .read(setFavoriteProductsProvider.notifier)
-                                .addToFavorites(product);
+                                .read(selectProductProvider.notifier)
+                                .selectProduct(product);
+
+                            GoRouter.of(context).push(
+                              '/products/${product.id}',
+                              extra: product,
+                            );
                           },
                         ),
-                        onTap: () {
-                          ref
-                              .read(selectProductProvider.notifier)
-                              .selectProduct(product);
-
-                          GoRouter.of(context).push(
-                            '/products/${product.id}',
-                            extra: product,
-                          );
-                        },
+                      );
+                    },
+                    loading: () => Shimmer.fromColors(
+                      baseColor: Colors.grey[300]!,
+                      highlightColor: Colors.grey[100]!,
+                      child: const ListTile(
+                        leading: CircleAvatar(),
+                        title: Text(''),
+                        subtitle: Text(''),
+                        trailing: Text(''),
                       ),
-                    );
-                  },
-                );
-              },
-              loading: () => const SliverFillRemaining(
-                child: Center(
-                  child: CircularProgressIndicator(),
-                ),
-              ),
-              error: (error, stackTrace) => SliverFillRemaining(
-                child: Center(
-                  child: Text('Error: $error'),
-                ),
+                    ),
+                    error: (error, stackTrace) => Center(
+                      child: Text('Error: $error'),
+                    ),
+                  );
+                },
               ),
             ),
           ],
