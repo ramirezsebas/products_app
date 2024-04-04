969102c0d5deb8352cfce6cd69b4f08813d06f11
Matias Ramirez <ramirezmatias946@gmail.com>
2024-04-04 15:07:40 -0400

    Add navigation to cart

--- lib/app/screens/product_screen.dart
+++ lib/app/screens/product_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:product_manager/app/models/product_model.dart';
 import 'package:product_manager/app/providers/providers.dart';
 import 'package:product_manager/app/widgets/product_view_section.dart';
@@ -33,18 +34,64 @@ class ProductScreen extends ConsumerWidget {
     return Scaffold(
       appBar: AppBar(),
       floatingActionButton: FloatingActionButton(
+        heroTag: null,
         onPressed: () {
-          ref
-              .read(setFavoriteProductsProvider.notifier)
-              .addToFavorites(productModel);
+          GoRouter.of(context).push('/products/favorites');
         },
-        child: const Icon(Icons.shopping_cart_rounded),
+        child: const Icon(
+          Icons.shopping_cart_checkout_rounded,
+        ),
       ),
       body: CustomScrollView(
         slivers: [
           SliverToBoxAdapter(
             child: ProductViewSection(
               productModel: productModel,
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                ListTile(
+                  title: const Text('Producto'),
+                  subtitle: Text(productModel.title),
+                ),
+                const Divider(),
+                ListTile(
+                  title: const Text('Descripcion'),
+                  subtitle: Text(productModel.description),
+                ),
+                const Divider(),
+                ListTile(
+                  title: const Text('Precio'),
+                  subtitle: Text(r'$' + productModel.price.toStringAsFixed(2)),
+                ),
+                const Divider(),
+                ListTile(
+                  title: const Text('Descuento'),
+                  subtitle: Text('${productModel.discountPercentage}%'),
+                ),
+                const Divider(),
+                ListTile(
+                  title: const Text('Marca'),
+                  subtitle: Text(productModel.brand),
+                ),
+                const Divider(),
+                ListTile(
+                  title: const Text('Categoria'),
+                  subtitle: Text(productModel.category),
+                ),
+                const Divider(),
+                ListTile(
+                  title: const Text('Rating'),
+                  subtitle: Text(productModel.rating.toString()),
+                ),
+                const Divider(),
+                ListTile(
+                  title: const Text('Stock'),
+                  subtitle: Text(productModel.stock.toString()),
+                ),
+              ],
             ),
           ),
         ],
