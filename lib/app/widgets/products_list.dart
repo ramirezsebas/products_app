0e61e54c1f58901ceabea2c7fe6d39e24b04e962
Matias Ramirez <ramirezmatias946@gmail.com>
2024-04-04 19:54:37 -0400

    Refactor ProductsList widget to include pull-to-refresh functionality and slidable actions

--- lib/app/widgets/products_list.dart
+++ lib/app/widgets/products_list.dart
@@ -19,106 +19,111 @@ class ProductsList extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ListView.custom(
-      childrenDelegate: SliverChildBuilderDelegate(
-        (context, index) {
-          const limit = 30;
-          final skip = (index ~/ limit) * limit;
-          final itemIndexInProducts = index % limit;
+    return RefreshIndicator(
+      onRefresh: () async {
+        return ref.invalidate(fetchProductsProvider());
+      },
+      child: ListView.custom(
+        childrenDelegate: SliverChildBuilderDelegate(
+          (context, index) {
+            const limit = 30;
+            final skip = (index ~/ limit) * limit;
+            final itemIndexInProducts = index % limit;
 
-          final products = ref.watch(
-            fetchProductsProvider(
-              skip: skip,
-            ),
-          );
+            final products = ref.watch(
+              fetchProductsProvider(
+                skip: skip,
+              ),
+            );
 
-          return products.when(
-            data: (data) {
-              if (itemIndexInProducts >= data.length) {
-                return null;
-              }
+            return products.when(
+              data: (data) {
+                if (itemIndexInProducts >= data.length) {
+                  return null;
+                }
 
-              final product = data[itemIndexInProducts];
-              return Slidable(
-                key: ValueKey(product.id),
-                endActionPane: ActionPane(
-                  motion: const ScrollMotion(),
-                  children: [
-                    SlidableAction(
-                      onPressed: (context) {
-                        addToFavorites(product);
-                      },
-                      backgroundColor: const Color(0xFF21B7CA),
-                      foregroundColor: Colors.white,
-                      icon: !checkIsFavorite(product)
-                          ? Icons.favorite_border_rounded
-                          : Icons.favorite_rounded,
-                      label: !ref
-                              .watch(
-                                setFavoriteProductsProvider.notifier,
-                              )
-                              .isFavorite(product)
-                          ? 'Add to favorites'
-                          : 'Remove from favorites',
-                    ),
-                  ],
-                ),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 25,
-                    backgroundImage: CachedNetworkImageProvider(
-                      product.thumbnail,
-                      errorListener: (exception) {
-                        debugPrint('Error: $exception');
-                      },
-                    ),
+                final product = data[itemIndexInProducts];
+                return Slidable(
+                  key: ValueKey(product.id),
+                  endActionPane: ActionPane(
+                    motion: const ScrollMotion(),
+                    children: [
+                      SlidableAction(
+                        onPressed: (context) {
+                          addToFavorites(product);
+                        },
+                        backgroundColor: const Color(0xFF21B7CA),
+                        foregroundColor: Colors.white,
+                        icon: !checkIsFavorite(product)
+                            ? Icons.favorite_border_rounded
+                            : Icons.favorite_rounded,
+                        label: !ref
+                                .watch(
+                                  setFavoriteProductsProvider.notifier,
+                                )
+                                .isFavorite(product)
+                            ? 'Add to favorites'
+                            : 'Remove from favorites',
+                      ),
+                    ],
                   ),
-                  title: Text(product.title),
-                  subtitle: Text(product.description),
-                  trailing: IconButton(
-                    icon: Icon(
-                      !ref
-                              .watch(
-                                setFavoriteProductsProvider.notifier,
-                              )
-                              .isFavorite(product)
-                          ? Icons.favorite_border_rounded
-                          : Icons.favorite_rounded,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 25,
+                      backgroundImage: CachedNetworkImageProvider(
+                        product.thumbnail,
+                        errorListener: (exception) {
+                          debugPrint('Error: $exception');
+                        },
+                      ),
                     ),
-                    onPressed: () {
+                    title: Text(product.title),
+                    subtitle: Text(product.description),
+                    trailing: IconButton(
+                      icon: Icon(
+                        !ref
+                                .watch(
+                                  setFavoriteProductsProvider.notifier,
+                                )
+                                .isFavorite(product)
+                            ? Icons.favorite_border_rounded
+                            : Icons.favorite_rounded,
+                      ),
+                      onPressed: () {
+                        ref
+                            .read(setFavoriteProductsProvider.notifier)
+                            .addToFavorites(product);
+                      },
+                    ),
+                    onTap: () {
                       ref
-                          .read(setFavoriteProductsProvider.notifier)
-                          .addToFavorites(product);
+                          .read(selectProductProvider.notifier)
+                          .selectProduct(product);
+
+                      GoRouter.of(context).push(
+                        '/products/${product.id}',
+                        extra: product,
+                      );
                     },
                   ),
-                  onTap: () {
-                    ref
-                        .read(selectProductProvider.notifier)
-                        .selectProduct(product);
-
-                    GoRouter.of(context).push(
-                      '/products/${product.id}',
-                      extra: product,
-                    );
-                  },
+                );
+              },
+              loading: () => Shimmer.fromColors(
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+                child: const ListTile(
+                  leading: CircleAvatar(),
+                  title: Text(''),
+                  subtitle: Text(''),
+                  trailing: Text(''),
                 ),
-              );
-            },
-            loading: () => Shimmer.fromColors(
-              baseColor: Colors.grey[300]!,
-              highlightColor: Colors.grey[100]!,
-              child: const ListTile(
-                leading: CircleAvatar(),
-                title: Text(''),
-                subtitle: Text(''),
-                trailing: Text(''),
               ),
-            ),
-            error: (error, stackTrace) => Center(
-              child: Text('Error: $error'),
-            ),
-          );
-        },
+              error: (error, stackTrace) => Center(
+                child: Text('Error: $error'),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
