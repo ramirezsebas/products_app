57ec77a1580ef34b25b8aa78dcb9b3db3800558b
Matias Ramirez <ramirezmatias946@gmail.com>
2024-04-04 13:49:10 -0400

    Add favorites screen to router

--- lib/app/core/router.dart
+++ lib/app/core/router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:product_manager/app/models/product_model.dart';
 import 'package:product_manager/app/screens/error_screen.dart';
+import 'package:product_manager/app/screens/favorites_screen.dart';
 
 import 'package:product_manager/app/screens/product_screen.dart';
 import 'package:product_manager/app/screens/products_screen.dart';
@@ -12,6 +13,12 @@ final router = GoRouter(
       path: '/products',
       builder: (context, state) => const ProductsScreen(),
       routes: [
+        GoRoute(
+          path: 'favorites',
+          builder: (context, state) {
+            return const FavoritesScreen();
+          },
+        ),
         GoRoute(
           path: ':id',
           builder: (context, state) {
