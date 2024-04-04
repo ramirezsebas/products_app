c458f5be35715949ddbe51f36359aa5a2c63e83a
Matias Ramirez <ramirezmatias946@gmail.com>
2024-04-04 13:49:15 -0400

    Add FavoritesScreen widget

---
+++ lib/app/screens/favorites_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:product_manager/app/providers/providers.dart';
+
+class FavoritesScreen extends ConsumerWidget {
+  const FavoritesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final products = ref.watch(setFavoriteProductsProvider);
+
+    return SafeArea(
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Favorites'),
+        ),
+        body: products.isEmpty
+            ? const Center(
+                child: Text('No favorites yet'),
+              )
+            : ListView.separated(
+                itemCount: products.length,
+                separatorBuilder: (context, index) => const Divider(),
+                itemBuilder: (context, index) {
+                  final product = products[index];
+
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(product.thumbnail),
+                    ),
+                    title: Text(product.title),
+                    subtitle: Text(product.description),
+                    trailing: Text(product.price.toString()),
+                    onTap: () {
+                      ref
+                          .read(selectProductProvider.notifier)
+                          .selectProduct(product);
+
+                      GoRouter.of(context).push(
+                        '/products/${product.id}',
+                        extra: product,
+                      );
+                    },
+                  );
+                },
+              ),
+      ),
+    );
+  }
+}
