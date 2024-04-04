f4ff156a9d46f4023b3a22fa45b96cbf87de2385
Matias Ramirez <ramirezmatias946@gmail.com>
2024-04-04 12:14:47 -0400

    Add router configuration for product management

---
+++ lib/app/core/router.dart
@@ -0,0 +1,32 @@
+import 'package:go_router/go_router.dart';
+import 'package:product_manager/app/models/product_model.dart';
+import 'package:product_manager/app/screens/error_screen.dart';
+
+import 'package:product_manager/app/screens/product_screen.dart';
+import 'package:product_manager/app/screens/products_screen.dart';
+
+final router = GoRouter(
+  initialLocation: '/products',
+  routes: [
+    GoRoute(
+      path: '/products',
+      builder: (context, state) => const ProductsScreen(),
+      routes: [
+        GoRoute(
+          path: ':id',
+          builder: (context, state) {
+            final extra = state.extra as ProductModel?;
+
+            if (extra == null) {
+              return const ErrorScreen();
+            }
+
+            return ProductScreen(
+              productModel: extra,
+            );
+          },
+        ),
+      ],
+    ),
+  ],
+);
