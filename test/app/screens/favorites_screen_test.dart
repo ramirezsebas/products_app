16ffca4ed8fde7ca849465d481673a77ea6c2d1d
Matias Ramirez <ramirezmatias946@gmail.com>
2024-04-04 17:48:45 -0400

    Add unit tests for FavoritesScreen

---
+++ test/app/screens/favorites_screen_test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'package:product_manager/app/repositories/products_repository.dart';
+import 'package:product_manager/app/repositories/users_repository.dart';
+import 'package:product_manager/app/screens/favorites_screen.dart';
+
+import '../../helpers/helpers.dart';
+
+class MockUserRepository extends Mock implements UsersRepository {}
+
+class MockProductsRepository extends Mock implements ProductsRepository {}
+
+void main() {
+  group('favorites screen ...', () {
+    testWidgets('should render correctly FavoritesScreen', (tester) async {
+      await tester.pumpApp(
+        const ProviderScope(
+          child: FavoritesScreen(),
+        ),
+      );
+
+      expect(find.byType(FavoritesScreen), findsOneWidget);
+    });
+
+    testWidgets(
+        'should render correctly FavoritesScreen with the text Favorites',
+        (tester) async {
+      await tester.pumpApp(
+        const ProviderScope(
+          child: FavoritesScreen(),
+        ),
+      );
+
+      expect(find.text('Favorites'), findsOneWidget);
+    });
+
+    testWidgets(
+        '''should render correctly FavoritesScreen with the text No favorites yet''',
+        (tester) async {
+      await tester.pumpApp(
+        const ProviderScope(
+          child: FavoritesScreen(),
+        ),
+      );
+
+      expect(find.text('No favorites yet'), findsOneWidget);
+    });
+  });
+}
